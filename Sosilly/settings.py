c5dca3812d751665890c427d51b670a0c354a076
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2022-09-25 18:19:53 +0530

    second commit

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -115,11 +115,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'network/static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'network/static'),
-    os.path.join(BASE_DIR, 'network/media')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'network/static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'network/static'),
+#     os.path.join(BASE_DIR, 'network/media')
+# ]
  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
 MEDIA_URL = '/media/' 
