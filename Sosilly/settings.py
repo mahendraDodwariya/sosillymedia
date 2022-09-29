5a81a11a1c4e92cad1b6e0f797bb470e0ebbdce7
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2022-09-29 18:29:16 +0530

    django-storages added

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = ["sosillymedia.herokuapp.com","localhost"]
 
 INSTALLED_APPS = [
     'network',
+    'storages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
