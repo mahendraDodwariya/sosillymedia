fac0ff233202dae91ab3c9a53980793391254eee
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2022-10-15 01:39:00 +0530

    rechecking

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,7 +114,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'network/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'network/staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'network/static'),
     os.path.join(BASE_DIR, 'network/media')
