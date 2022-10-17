228edc528153d49ace0ef7ec0fda90437d48e71d
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2022-10-18 01:14:57 +0530

    postgresql

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -74,6 +74,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = "network.User"
 
