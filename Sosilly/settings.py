40c0b3c6ca17cf879f438b9dc1a3b0ed7940b767
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2023-01-03 00:11:34 +0530

    deploying to railway

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sosillymedia.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,5 +125,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'network/media')
 ]
  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
