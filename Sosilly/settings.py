f3d92301c5ed4a0452ba1b0e366b229507726dd1
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2023-01-05 21:56:19 +0530

    token update

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://sosillymedia.up.railway.app']
 
