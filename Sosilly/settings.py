46fa83cc1e6493f57aa92bba7a2bf56ddb0759dd
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2023-01-05 22:03:23 +0530

    csrf token 3rd try

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# if not DEBUG:
-#     CSRF_TRUSTED_ORIGINS = ['https://sosillymedia.up.railway.app']
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['https://sosillymedia.up.railway.app']
 
 ALLOWED_HOSTS = ["*"]
 
