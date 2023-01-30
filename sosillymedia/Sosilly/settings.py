ca5216cace352232c2a117921118ff006fd30f81
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2023-01-31 01:06:29 +0530

    certain changes

--- sosillymedia/Sosilly/settings.py
+++ sosillymedia/Sosilly/settings.py
@@ -14,6 +14,8 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# if not DEBUG:
+#     CSRF_TRUSTED_ORIGINS = ['https://sosillymedia.up.railway.app']
 
 ALLOWED_HOSTS = ['*']
 
