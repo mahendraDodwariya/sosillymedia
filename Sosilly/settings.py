87b29d60fc84a33a1ed79621010fd886792fc54b
Mahendra dodwariya <mahendradodwariya16032004@gmail.com>
2023-03-19 21:36:33 +0530

    final to deploy

--- Sosilly/settings.py
+++ Sosilly/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sosillytrial.com","localhost"]
+# ALLOWED_HOSTS = ["sosillytrial.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
